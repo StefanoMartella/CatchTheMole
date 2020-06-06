3f7f90fcc6c80e544176b5641f0025fe114f5eb1
StefanoMartella <stefano.martella9614@gmail.com>
2020-06-06 15:35:21 +0200

    From 10s to 30s

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@ function Mole(element, dimension) {
     actualRecordLabel.innerText = `Record: ${storedRecord ? storedRecord : '0'} moles`;
 
     let countdownLabel = document.createElement('span');
-    countdownLabel.innerText = '10';
+    countdownLabel.innerText = '30';
     countdownLabel.id = 'countdown';
     countdownLabel.style.fontWeight = 'bold';
     countdownLabel.style.fontSize = '25px';
@@ -208,7 +208,7 @@ function Mole(element, dimension) {
   }
 
   function startGame(startButton) {
-    let countdown = 10;
+    let countdown = 30;
     score = 0;
     startButton.disabled = true;
     startButton.style.opacity = '0.5';
